d13cb2c1a0765af9c334ad3f2cf7f97d6e1de20b
Vish Rajagopalan <vishrajagopalan@cloudera.com>
2024-04-20 15:45:37 +0000

    added sentence window RAG

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -58,7 +58,7 @@ def is_process_running(process_name):
   """
   for proc in psutil.process_iter(['name']):
     try:
-      if process_name.lower() in proc.info['name'].lower():
+      if process_name.lower() in proc.info['name'].lower() and proc.status <> "zombie":
         return True
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
       pass
@@ -104,7 +104,7 @@ elapsed_time = end_time - start_time
 logger.info(f"INFO: Model Loaded and response, Time required: {elapsed_time:.6f} seconds")
 
 # make sure litellm works
-from litellm import completion
+#from litellm import completion
 #response = completion(
 #    model="ollama/llama2", 
 #    messages=[{ "content": "respond in 20 words. who are you?","role": "user"}], 
@@ -119,7 +119,9 @@ import ollama
 
 logger.info("INFO : Inside On chat start")
 #Let us set up our LLM Now
-Settings.llm = Ollama(model="llama2", keep_alive=-1, time_out=120)
+#Settings.llm = Ollama(model="llama2", keep_alive=-1)
+Settings.llm = Ollama(model="llama2", request_timeout=1000)
+
 Settings.llm.base_url="http://127.0.0.1:8080"
 
 logger.info("INFO: Running the Bootstrap")
@@ -200,39 +202,87 @@ f_context_relevance = (
 )
 
 from trulens_eval import TruLlama
+feedbacks=[f_groundedness, f_answer_relevance, f_context_relevance]
+
 tru_query_engine_recorder = TruLlama(query_engine,
     app_id='LlamaIndex_App1',
-    feedbacks=[f_groundedness, f_answer_relevance, f_context_relevance])
+    feedbacks=feedbacks)
 
-## or as context manager
-with tru_query_engine_recorder as recording:
-    query_engine.query("What did the author do growing up?")
-  
-# The record of the app invocation can be retrieved from the `recording`:
-
-rec = recording.get() # use .get if only one record
-# recs = recording.records # use .records if multiple
-
-display(rec)
+### or as context manager
+#with tru_query_engine_recorder as recording:
+#    query_engine.query("What did the author do growing up?")
+#  
+## The record of the app invocation can be retrieved from the `recording`:
+#
+#rec = recording.get() # use .get if only one record
+## recs = recording.records # use .records if multiple
+#
+#display(rec)
 
 # The results of the feedback functions can be rertireved from
 # `Record.feedback_results` or using the `wait_for_feedback_result` method. The
 # results if retrieved directly are `Future` instances (see
 # `concurrent.futures`). You can use `as_completed` to wait until they have
 # finished evaluating or use the utility method:
-
-for feedback, feedback_result in rec.wait_for_feedback_results().items():
-    print(feedback.name, feedback_result.result)
+#
+#for feedback, feedback_result in rec.wait_for_feedback_results().items():
+#    print(feedback.name, feedback_result.result)
 
 # See more about wait_for_feedback_results:
 # help(rec.wait_for_feedback_results)
+#records, feedback = tru.get_records_and_feedback(app_ids=["LlamaIndex_App1"])
+#
+#records.head()
+#
+#tru.get_leaderboard(app_ids=["LlamaIndex_App1"])
+
+# go through a list of questions for a baseline
+
+from trulens_eval import Tru
+def run_evals(eval_questions, tru_recorder, query_engine):
+    for question in eval_questions:
+        with tru_recorder as recording:
+            response = query_engine.query(question)
+            
+eval_questions = []
+with open('./data/questions/paul_graham_eval_questions.txt', 'r') as file:
+    for line in file:
+        # Remove newline character and convert to integer
+        item = line.strip()
+        eval_questions.append(item)
+
+display(eval_questions)
+for question in eval_questions:
+    with tru_query_engine_recorder as recording:
+        query_engine.query(question)
+        
+import pandas as pd
 records, feedback = tru.get_records_and_feedback(app_ids=["LlamaIndex_App1"])
+pd.set_option("display.max_colwidth", None)
+records[["input", "output"] + feedback]
 
-records.head()
-
-tru.get_leaderboard(app_ids=["LlamaIndex_App1"])
-
-
+tru.get_leaderboard(app_ids=[])
 
 
-  
+
+# Let us go through these guestions now for sentencewindowNode Parser
+from  utils.rag_helper import *
+sentence_window_index_1 = build_sentence_window_index(documents, save_dir="./data/index/sentence_index1", window_size=1)
+sentence_window_engine_1 = get_sentence_window_query_engine( sentence_window_index_1)
+tru_recorder_1 = get_prebuilt_trulens_recorder(
+    sentence_window_engine_1,
+    app_id='sentence window engine 1', feedbacks=feedbacks
+)
+
+tru.get_leaderboard(app_ids=[])
+
+
+# let us go through a window size of 3
+sentence_window_index_3 = build_sentence_window_index(documents, save_dir="./data/index/sentence_index3", window_size=3)
+sentence_window_engine_3 = get_sentence_window_query_engine( sentence_window_index_3)
+tru_recorder_3 = get_prebuilt_trulens_recorder(
+    sentence_window_engine_3,
+    app_id='sentence window engine 3', feedbacks=feedbacks
+)
+run_evals(eval_questions, tru_recorder_3, sentence_window_engine_3)
+tru.get_leaderboard(app_ids=[])
